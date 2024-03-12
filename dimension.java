e3717af4c200dbf728e1852a2ff850b0a49e7293
m.vijaya lakshmi <153913507+vidhya123sunny@users.noreply.github.com>
2024-03-12 20:45:31 +0530

    Add files via upload

---
+++ dimension.java
@@ -0,0 +1,21 @@
+public class dimension{
+    public static void main(String[] args){
+        int arr[][]=new int[2][2];
+        arr[0][0]=1;
+        arr[0][1]=1;
+        arr[1][0]=1;
+        arr[1][1]=1;
+          
+        int arr2[][]={{1,2,3},{1,2,3},{1,2,3}};
+        for(int i=0;i<2;i++){
+         for(int j=0;j<2;j++){
+             System.out.println(arr[i][j]);
+            }
+        }
+          
+    }
+}
+        
+        
+        
+
